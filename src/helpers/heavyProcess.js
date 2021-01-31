6764318c0d8bd452c48c0a634e539251bb8422f2
Philip Pérez Castro <jperez.c92@gmail.com>
2021-01-30 23:35:04 -0500

    MemoHook.js added

---
+++ src/helpers/heavyProcess.js
@@ -0,0 +1,9 @@
+const heavyProcess = (iterations) => {
+  for (let index = 0; index < iterations; index++) {
+    console.log(`ahí vamos`);
+  }
+
+  return `${iterations} iteraciones realizadas`;
+};
+
+export default heavyProcess;
