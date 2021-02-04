389e2b7a33bc9fa8aa0d1c52881b2f5c3e76fbf8
Philip Pérez Castro <jperez.c92@gmail.com>
2021-02-04 00:37:29 -0500

    added tests for useCounter

--- src/tests/hooks/useCounter.test.js
+++ src/tests/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { act, renderHook } from "@testing-library/react-hooks";
 import useCounter from "../../hooks/useCounter";
 
 describe("Pruebas en el archivo useCounter", () => {
@@ -9,5 +9,46 @@ describe("Pruebas en el archivo useCounter", () => {
     expect(typeof result.current.increment).toBe("function");
     expect(typeof result.current.decrement).toBe("function");
     expect(typeof result.current.reset).toBe("function");
+  });
+
+  test("Debe msotrar el counter en 101", () => {
+    const count = 101;
+    const { result } = renderHook(() => useCounter(count));
+
+    expect(result.current.counter).toBe(count);
+  });
+
+  test("Debe de aumentar el counter en 1", () => {
+    const { result } = renderHook(() => useCounter(101));
+    const { increment } = result.current;
+
+    act(() => {
+      increment();
+    });
+
+    expect(result.current.counter).toBe(102);
+  });
+
+  test("Debe de disminuir el counter en 1", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { decrement } = result.current;
+
+    act(() => {
+      decrement();
+    });
+
+    expect(result.current.counter).toBe(99);
+  });
+
+  test("Debe de restablecer el counter en 100", () => {
+    const { result } = renderHook(() => useCounter(101));
+    const { decrement, reset } = result.current;
+
+    act(() => {
+      decrement();
+      reset();
+    });
+
+    expect(result.current.counter).toBe(101);
   });
 });
