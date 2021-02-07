3962ceb76d0e741c6eb356442c2a195a26b755c6
Philip Perez Castro <jperez.c92@gmail.com>
2021-02-07 00:28:10 -0500

    added tests for todoReducer

--- src/tests/components/08-useReducer/todoReducer.test.js
+++ src/tests/components/08-useReducer/todoReducer.test.js
@@ -7,9 +7,22 @@ describe("Pruebas en el archivo todoReducer", () => {
     expect(state).toEqual(demoTodos);
   });
 
-  test("Debe de agregar un todo", () => {
+  test("Debe de agregar un TODO", () => {
     const state = todoReducer(demoTodos, { type: "ADD", payload: newTodo });
     expect(state).toEqual([...demoTodos, newTodo]);
     expect(state.length).toBe(3);
+  });
+
+  test("Debe de borrar un TODO", () => {
+    const state = todoReducer(demoTodos, { type: "DELETE", payload: 2 });
+
+    expect(state.length).toBe(1);
+    expect(state).toEqual([demoTodos[0]]);
+  });
+
+  test("Debe de cambiar un TODO", () => {
+    const state = todoReducer(demoTodos, { type: "TOGGLE", payload: 1 });
+    expect(state[1]).toEqual(demoTodos[1]);
+    expect(state[0].done).toBe(true);
   });
 });
