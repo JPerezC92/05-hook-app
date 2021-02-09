39f34e697ce6522a41f8ab36f644814ac3a4f54e
Philip Perez Castro <jperez.c92@gmail.com>
2021-02-09 00:02:06 -0500

    added tests for HomeScreen

---
+++ src/tests/components/09-useContext/HomeScreen.test.js
@@ -0,0 +1,16 @@
+import { mount } from "enzyme";
+import HomeScreen from "../../../components/09-useContext/HomeScreen";
+import UserContext from "../../../components/09-useContext/UserContext";
+
+describe("Pruebas en el componente <HomeScreen />", () => {
+  const user = { name: "Philip", email: "perez@gmail.com" };
+
+  const wrapper = mount(
+    <UserContext.Provider value={{ user }}>
+      <HomeScreen />
+    </UserContext.Provider>
+  );
+  test("Debe de mostrarse correctamente", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
